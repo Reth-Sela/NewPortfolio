22eee008d8ae975045e11252b4cd3bf30d6418ab
Reth-Sela <selareth7@gmail.com>
2024-08-21 11:30:46 +0700

    delete urlLink and create linktourl

--- src/component/GroupLink.jsx
+++ src/component/GroupLink.jsx
@@ -7,27 +7,24 @@ import tiktok from "../assets/tiktok.png";
 import ig from "../assets/igblack.png";
 import telegram from "../assets/telegramblack.png";
 import skype from "../assets/skype.png";
-import UrlLink from "./urlLink";
+import LinkToUrl from "./LinkToUrl";
 
 const GroupLink = () => {
   return (
     <div className="p-[5px] block">
-      <Alink text={"PORTFOLIO"} imgSrc={portfolio} To={'header'} />
+      <Alink text={"PORTFOLIO"} imgSrc={portfolio} To={"header"} />
 
-      <Alink text={"ABOUT"} imgSrc={about} To={'about'}/>
-      <Alink text={"CONTACT"} imgSrc={contact} To={'contact'} />
+      <Alink text={"ABOUT"} imgSrc={about} To={"about"} />
+      <Alink text={"CONTACT"} imgSrc={contact} To={"contact"} />
       <div className="flex justify-start">
-      
-      
-        
-        <UrlLink
+        <LinkToUrl
           imgSrc={facebook}
           href={"https://www.facebook.com/sela.reth.75"}
         />
-        <UrlLink imgSrc={tiktok} href={"https://www.tiktok.com/@sela_reth"} />
-        <UrlLink imgSrc={ig} href={"https://www.instagram.com/reth.sela"} />
-        <UrlLink imgSrc={telegram} href={"https://t.me/Selareth"} />
-        <UrlLink
+        <LinkToUrl imgSrc={tiktok} href={"https://www.tiktok.com/@sela_reth"} />
+        <LinkToUrl imgSrc={ig} href={"https://www.instagram.com/reth.sela"} />
+        <LinkToUrl imgSrc={telegram} href={"https://t.me/Selareth"} />
+        <LinkToUrl
           imgSrc={skype}
           href={"https://join.skype.com/invite/rsqk95Pe1ZDb"}
         />
